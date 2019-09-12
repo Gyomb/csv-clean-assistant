bb0978902bacab6857681c7a830be2942636cecb
Unknown <gyomb.graphics@gmail.com>
2019-09-12 23:41:29 +0200

    add routes and bulma

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 
+import 'bulma/css/bulma.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+import router from './router'
+
 Vue.config.productionTip = false
 
 new Vue({
   store,
+  router,
   render: h => h(App)
 }).$mount('#app')
