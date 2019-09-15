7474af194cc9c9d212180854df0dd433c7347cc9
Unknown <gyomb.graphics@gmail.com>
2019-09-15 19:30:19 +0200

    Added content and status storage in the csv store

--- src/store/modules/csv.js
+++ src/store/modules/csv.js
@@ -1,16 +1,35 @@
 import { ipcRenderer } from 'electron'
+import Vue from 'vue'
 
-const state = {}
+const state = {
+  csvRequestStatus: undefined,
+  csvContent: undefined,
+  encoding: 'utf-8'
+}
 
-const mutations = {}
+const mutations = {
+  CSV_STATUS_UPDATE (state, status) {
+    Vue.set(state, 'csvRequestStatus', status)
+  },
+  CSV_CONTENT_UPDATE (state, content) {
+    Vue.set(state, 'csvContent', content)
+  },
+  CSV_ENCODING_UPDATE (state, encoding) {
+    state.encoding = encoding
+  }
+}
 
 const actions = {
-  ANALYZE_CSV (context, path) {
-    ipcRenderer.once('analyzedCsv', (event, path) => {
-      console.log('analyzedCsv', path)
+  ANALYZE_CSV ({ commit }, path) {
+    commit('CSV_STATUS_UPDATE', 'analyzing')
+    ipcRenderer.once('analyzedCsv', (event, content) => {
+      commit('CSV_CONTENT_UPDATE', content.file)
+      commit('CSV_ENCODING_UPDATE', content.encoding)
+      commit('CSV_STATUS_UPDATE', 'ready')
     })
     ipcRenderer.once('csvReadError', (event, msg) => {
-      console.error('csvReadError', msg)
+      console.error({ csvReadError: msg })
+      commit('CSV_STATUS_UPDATE', msg)
     })
     ipcRenderer.send('analyzeCsv', path)
   }
