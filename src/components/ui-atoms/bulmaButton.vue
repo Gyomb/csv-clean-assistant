8c69e5af1ef1e47c38f1e5de2acaf74496059c4d
Unknown <gyomb.graphics@gmail.com>
2019-09-17 01:15:31 +0200

    added an atom component : bulmaButton
    For future use

---
+++ src/components/ui-atoms/bulmaButton.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="button is-small is-rounded" :class="[colorClass]" @click="$emit('click')">
+    <span class="icon is-small" v-if="picto">
+        <i class="fas fa-play fa-inverse" :class="[iconClass]"></i>
+    </span>
+    <span v-if="label">{{label}}</span>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'bulmaButton',
+  props: {
+    picto: String,
+    purpose: String,
+    label: String
+  },
+  computed: {
+    iconClass () {
+      return 'fa-' + this.picto
+    },
+    colorClass () {
+      return 'is-' + this.purpose
+    }
+  }
+}
+</script>
