5e290376f0ab6a59873157e0b2f85e0d29a8e164
Guillaume Barbier <gyomb.graphics@gmail.com>
2019-09-17 14:04:39 +0200

    Bulma file-input proposal The base appearance is set up but the control isn't functional yet.

---
+++ src/components/ui-atoms/bulmaFileSelect.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="field">
+    <div class="file has-name">
+      <label :for="fieldId" class="file-label">
+        <input type="file" :name="fieldId" :id="fieldId" class="file-input">
+      </label>
+      <span class="file-cta is-primary">
+        <span class="file-icon">
+          <i :class="faPicto"></i>
+        </span>
+        <span :v-if="label" class="file-label">
+          {{label}}
+        </span>
+      </span>
+      <span class="file-name">
+        {{filenameDisplay}}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'bulmaFileSelect',
+  data(){
+    return {
+      filename: ''
+    }
+  },
+  props :{
+    fieldId: String,
+    picto: {
+      String,
+      default: 'search'
+    },
+    label: { default: '' },
+    placeholder: {
+      type: String,
+      default: 'Select a folder...'
+    }
+  },
+  computed: {
+    faPicto () {
+      return 'fas fa-'+this.picto
+    },
+    filenameDisplay () {
+      return this.filename ? this.filename : this.placeholder
+    }
+  }
+}
+</script>
