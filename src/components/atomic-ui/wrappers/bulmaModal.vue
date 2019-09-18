edd798c42a17021afec3d6fab2eb2195d8642937
Unknown <gyomb.graphics@gmail.com>
2019-09-19 00:53:46 +0200

    added atomic-ui/wrappers/bulmaModal
    a wrapper to easily create modals

---
+++ src/components/atomic-ui/wrappers/bulmaModal.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="modal">
+    <div class="modal-background"></div>
+    <div :class="isCard ? 'modal-card' : 'modal-content'">
+      <template v-if="isCard">
+        <header class="modal-card-head">
+          <div class="modal-card-title" v-if="hasHeader">
+            <slot name="header"></slot>
+          </div>
+          <button class="delete" aria-label="close" @click="close()"></button>
+        </header>
+        <section class="modal-card-body">
+          <slot></slot>
+        </section>
+        <footer class="modal-card-foot" v-if="hasFooter">
+          <slot name="footer"></slot>
+        </footer>
+      </template>
+      <div v-else :class="{box: !noBox}">
+        <slot></slot>
+      </div>
+    </div>
+    <button v-if="!isCard" class="modal-close is-large" aria-label="close" @click="close()"></button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bulmaModal',
+  props: {
+    noBox: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    hasHeader () {
+      return !!this.$slots.header
+    },
+    hasFooter () {
+      return !!this.$slots.footer
+    },
+    isCard () {
+      return this.hasHeader || this.hasHeader
+    }
+  },
+  methods: {
+    close () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
