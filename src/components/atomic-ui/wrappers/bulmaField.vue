187fced1c8e4504a7f649cc1f5301aa7521281d1
Unknown <gyomb.graphics@gmail.com>
2019-09-20 01:38:34 +0200

    changed the purpose of the bulmaField component
    The purpose was to wrap as many control as we want and put them grouped by default.
    I thought then than non-grouped component would behave fine with multiple controls.
    
    By default, the bulmaField is not grouped and should contain only one label and one control.
    
    Unless the `has-addons`attribute is added on the component, `is-grouped` is applied as a class on the field
    => can insert as many fields as necessary
    
    if the `has-addons`attribute is added on the component, the `has-addons` class is added to the field
    => can insert as many fields as necessary

--- src/components/atomic-ui/wrappers/bulmaField.vue
+++ src/components/atomic-ui/wrappers/bulmaField.vue
@@ -1,20 +1,42 @@
 <script>
+const defaultBoolean = {
+  type: Boolean,
+  default: false
+}
+
 export default {
   name: 'bulmaField',
   props: {
-    isStacked: {
-      type: Boolean,
-      default: false
-    }
+    isGrouped: defaultBoolean,
+    hasAddons: defaultBoolean
   },
   render (h) {
+    const rootNodeClass = () => {
+      if (this.hasAddons) return 'has-addons'
+      if (this.isGrouped) return 'is-grouped'
+    }
     const controls = this.$slots.default.map(vNode => {
-      return h('p', { class: 'control' }, [vNode])
+      if (vNode.tag === 'label') {
+        if (vNode.data) {
+          vNode.data.staticClass = [
+            'label',
+            vNode.data.staticClass || ''
+          ].join(' ')
+        } else {
+          vNode.data = {
+            staticClass: 'label'
+          }
+        }
+        return vNode
+      } else {
+        return h('p', { class: 'control' }, [vNode])
+      }
     })
+
     return h(
       'div',
       {
-        class: [ 'field', { 'is-grouped': !this.isStacked } ]
+        class: [ 'field', rootNodeClass() ]
       },
       controls
     )
