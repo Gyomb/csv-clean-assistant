1177b14f68e8dbadb60319354af3230788e21d3f
Unknown <gyomb.graphics@gmail.com>
2019-09-22 19:20:13 +0200

    sveFileControls : UI update
    added the superRadioButton component and rewired the fieldsets to behave like regular inputs.
    NOTE : also added computed properties that exploits the data from the superRadioButton and the forms to generate the correct path

--- src/components/saveFileControls.vue
+++ src/components/saveFileControls.vue
@@ -1,56 +1,96 @@
 <template>
   <div>
     <bulmaField has-addons>
-      <bulma-button picto="save" purpose="success" @click="saveCsv()" />
+      <bulma-button picto="save" purpose="success" @click="saveCsv()" :disabled="saveFilePath === ''"/>
       <bulma-button picto="search" @click="fileModalIsActive = true" />
       <span class="button is-rounded" @click="fileModalIsActive = true">{{$store.state.csv.newFilename}}</span>
     </bulmaField>
     <bulmaModal :is-active="fileModalIsActive" @close="fileModalIsActive = false">
-      <h2 slot="header">Choose where to save your file&hellip;</h2>
-      <bulmaField>
-        <label>In a new file :</label>
-        <bulmaFileSelect class="is-fullwidth"
-          select-type="folder" picto="folder" placeholder display="path"
-          :value="folderpath" @select="folderSelected"
-        />
-      </bulmaField>
-      <bulmaField>
-        <input type="text" class="input" placeholder="Enter a filename...">
-      </bulmaField>
-      <hr>
-      <bulmaField>
-        <label>In an existing file :</label>
-        <bulmaFileSelect
-          picto="file" placeholder
-          :value="filename" @select="fileSelected"
-        />
-      </bulmaField>
-      <bulma-button slot="footer" picto="save" purpose="success" label="Save" @click="saveCsv()" />
+      <h2 slot="header">Choose how to save your CSV&hellip;</h2>
+      <superRadioButtons v-model="saveMode"
+        :options="[
+          {label: 'Create a new file', value: 'newfile'},
+          {label: 'Overwrite an existing file', value: 'overwrite'}
+        ]"
+      >
+        <bulmaField slot="newfile">
+          <bulmaFileSelect class="is-fullwidth"
+            select-type="folder" picto="folder" placeholder display="path"
+            v-model="newfileFolderData"
+          />
+        </bulmaField>
+        <bulmaField slot="newfile">
+          <input type="text" class="input" placeholder="Enter a filename..." v-model="newfileFilename">
+        </bulmaField>
+        <bulmaField slot="overwrite">
+          <!-- <label>Overwrite an existing file :</label> -->
+          <bulmaFileSelect class='is-fullwidth'
+            picto="file" placeholder accept=".csv"
+            v-model="overwriteFileData"
+          />
+        </bulmaField>
+      </superRadioButtons>
+      <bulma-button slot="footer" picto="save" purpose="success" label="Save" @click="saveCsv()" :disabled="saveFilePath === ''" />
       <bulma-button slot="footer" picto="times" label="Cancel" @click="fileModalIsActive = false"/>
     </bulmaModal>
   </div>
 </template>
 
 <script>
+import superRadioButtons from '@/components/ui-toolbox/superRadioButtons'
 
 export default {
   name: 'save-file-controls',
+  components: {
+    superRadioButtons
+  },
   data () {
     return {
       fileModalIsActive: false,
-      filename: '',
-      folderpath: ''
+      saveMode: this.$store.state.csv.saveMode,
+      newfileFilename: '',
+      newfileFolderData: { displayValue: '', files: [] },
+      overwriteFileData: { displayValue: '', files: [] }
+    }
+  },
+  computed: {
+    newfilePath () {
+      try {
+        return this.newfileFolderData.files[0].path
+      } catch {
+        return ''
+      }
+    },
+    overwritePath () {
+      try {
+        return this.overwriteFileData.files[0].path
+      } catch {
+        return ''
+      }
+    },
+    saveFilePath () {
+      function addExtension (path) {
+        return path.replace(/(?:\.csv)*$/, '.csv')
+      }
+      let completePath = ''
+      switch (this.saveMode) {
+        case 'overwrite' :
+          completePath = this.overwritePath ? addExtension(this.overwritePath) : ''
+          break
+        case 'newfile' :
+          if (this.newfilePath === '' || this.newfileFilename === '') return ''
+          let pathDelimiter = this.newfilePath.includes('\\') ? `\\` : '/' // use 'slash' as folder ending, except on windows, if the fullpath was provided
+          completePath = addExtension(this.newfilePath + pathDelimiter + this.newfileFilename)
+          break
+      }
+      return completePath
     }
   },
   methods: {
     saveCsv () {
-      this.$store.dispatch('SAVE_CSV', 'filename_test')
-    },
-    folderSelected (folder) {
-      this.folderpath = folder.value
-    },
-    fileSelected (file) {
-      this.filename = file.value
+      if (typeof this.saveFilePath === 'string' && this.saveFilePath !== '') {
+        this.$store.dispatch('SAVE_CSV', this.saveFilePath)
+      }
     }
   }
 }
