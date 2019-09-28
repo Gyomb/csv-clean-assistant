7a426c3c9b13c07da213a89ddb95c61d04b58e56
Unknown <gyomb.graphics@gmail.com>
2019-09-28 22:41:35 +0200

    Added bulmaLevel wrapper

---
+++ src/components/atomic-ui/wrappers/bulmaLevel.vue
@@ -0,0 +1,39 @@
+<script>
+
+export default {
+  name: 'bulmaLevel',
+  props: {
+    mobileView: Boolean
+  },
+  computed: {
+    rootClasses () {
+      let classes = ['level']
+      if (this.mobileView) classes.push('is-mobile')
+      return classes.join(' ')
+    }
+  },
+  render (h) {
+    function wrapItems (slot) {
+      if (Array.isArray(slot)) {
+        return slot.map(vNode => h('div', { class: 'level-item' }, [vNode]))
+      }
+      return []
+    }
+    const leftItem = wrapItems(this.$slots.left)
+    const defaultItem = wrapItems(this.$slots.default)
+    const rightItem = wrapItems(this.$slots.right)
+
+    const leftPart = leftItem.length > 0 ? <div class="level-left">{leftItem}</div> : ''
+    const rightPart = rightItem.length > 0 ? <div class="level-right">{rightItem}</div> : ''
+
+    const template =
+      <div class={this.rootClasses}>
+        { leftPart }
+        { defaultItem }
+        { rightPart }
+      </div>
+
+    return template
+  }
+}
+</script>
