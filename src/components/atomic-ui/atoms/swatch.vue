1c065de81b7e38a6f98b84f4c7c248e3841e9bd3
Unknown <gyomb.graphics@gmail.com>
2019-09-29 18:54:13 +0200

    swatchSelector: custom input to select colors

---
+++ src/components/atomic-ui/atoms/swatch.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="`column is-${width}`">
+    <div
+      :class="[
+        'swatch',
+        color ? `has-background-${color}` : '',
+        { 'is-clickable': clickable }
+      ]"
+      @click="clickEventIfClickable"
+    >
+      <span class="icon is-medium" v-if="picto">
+        <i :class="`far fa-${picto} fa-inverse`"></i>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'swatch',
+  props: {
+    width: {
+      type: [String, Number],
+      default: 2
+    },
+    color: String,
+    picto: String,
+    clickable: Boolean
+  },
+  methods: {
+    clickEventIfClickable () {
+      if (this.clickable) this.$emit('click')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .swatch {
+    padding: calc(100% / 6);
+    border-radius: 1rem;
+    text-align: center;
+    .is-clickable {
+      cursor: pointer;
+    }
+  }
+  .is-1 > .swatch {
+    padding: calc(100% / 12);
+  }
+  .is-3 > .swatch {
+    padding: calc(100% / 4);
+  }
+  .is-4 > .swatch {
+    padding: calc(100% / 3);
+  }
+  .is-6 > .swatch {
+    padding: calc(100% / 4);
+  }
+  .is-12 > .swatch {
+    padding: calc(100% / 4);
+  }
+</style>
---
+++ src/components/atomic-ui/molecules/swatchSelect.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="swatch-selector columns is-multiline">
+    <swatch v-for="(option, index) in options" :key="index"
+      :color="option" :picto="value === option ? 'check-circle' : 'empty'"
+      clickable @click="$emit('input', option)"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'swatchSelector',
+  props: {
+    options: {
+      type: Array,
+      validator (optionList) {
+        return optionList.every(option => typeof option === 'string')
+      },
+      default () {
+        return [
+          'primary',
+          'info',
+          'link',
+          'success',
+          'warning',
+          'danger'
+        ]
+      }
+    },
+    value: {
+      type: String,
+      default () { return this.options[0] }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+  .swatch-selector .swatch {
+    cursor: pointer;
+    &:hover .fa-empty:before {
+      content: "\f111";
+    }
+  }
+</style>
