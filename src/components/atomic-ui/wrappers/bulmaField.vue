19e27cccaaac4e572c075f430e2ce57abb500b20
Unknown <gyomb.graphics@gmail.com>
2019-10-01 02:17:46 +0200

    bulmaField render refacto : multi label support & better is-horizontal
    previous render function was breaking the order of the slot's content, grouping labels and inputs aparts.
    
    Now the render function, whereas more complex is able to regroup inputs together (for is-horizontal grouping on field-label and field-body)
    
    in the case of is-horizontal field, each time a label is encountered, a new field is created with this label as first element

--- src/components/atomic-ui/wrappers/bulmaField.vue
+++ src/components/atomic-ui/wrappers/bulmaField.vue
@@ -23,7 +23,11 @@ export default {
       }
       return classes.join(' ')
     }
-    const labels = this.$slots.default.filter(vNode => vNode.tag === 'label').map(vNode => {
+    const formattedSlots = [[]]
+    let slotIndex = 0
+    let fieldLabelCount = 0
+
+    const formatLabels = (vNode) => {
       if (vNode.data) {
         if (vNode.data.staticClass && !vNode.data.staticClass.includes('label')) {
           vNode.data.staticClass = [
@@ -41,37 +45,99 @@ export default {
       } else {
         return vNode
       }
-    })
-    let controls = this.$slots.default.filter(vNode => vNode.tag !== 'label').map(vNode => {
-      return h('p', { class: 'control' }, [vNode])
-    })
-    if (this.isHorizontal) {
-      const fieldBodyClass = () => {
-        if (this.hasAddons) return 'has-addons'
-        if (this.isGrouped) return 'is-grouped'
-        return ''
-      }
-      if (fieldBodyClass === '') {
-        controls = h('div', { class: 'field-body' }, [
-          controls.map(vNode => h('div', { class: 'field' }, [vNode]))
-        ])
-      } else {
-        controls = h('div', { class: 'field-body' }, [
-          h('div', { class: `field ${fieldBodyClass()}` }, controls)
-        ])
-      }
     }
 
-    return h(
-      'div',
-      {
-        class: [ 'field', rootNodeClass() ]
-      },
-      [
-        ...labels,
-        controls
-      ]
-    )
+    const addField = (vNode, additionalClasses) => {
+      const addition = additionalClasses ? ' ' + additionalClasses : ''
+      return h('div', { class: 'field' + addition }, [vNode])
+    }
+
+    const addControl = vNode => {
+      return h('p', { class: 'control' }, [vNode])
+    }
+
+    const formatInput = input => {
+      return addField(addControl(input))
+    }
+
+    const concatNextInputs = (nodeIndex, slots) => {
+      let fieldBodyClass = ''
+      if (this.isGrouped) fieldBodyClass = 'is-grouped'
+      if (this.hasAddons) fieldBodyClass = 'has-addons'
+
+      const nextSlots = slots.slice(nodeIndex)
+
+      const concatenatedInput = []
+      let nextIndex = 0
+      let encounteredLabel = false
+      while (nextIndex < nextSlots.length && !encounteredLabel) {
+        if (nextSlots.hasOwnProperty(nextIndex)) {
+          const input = nextSlots[nextIndex]
+          if (input.tag === 'label') {
+            encounteredLabel = true
+            slotIndex--
+          } else {
+            concatenatedInput.push(input)
+            slots.splice(nodeIndex, 1)
+          }
+        }
+        nextIndex++
+      }
+
+      const formattedInputs = fieldBodyClass === ''
+        ? concatenatedInput.map(vNode => formatInput(vNode))
+        : [addField(concatenatedInput.map(vNode => addControl(vNode)), fieldBodyClass)]
+
+      return h('div', { class: 'field-body' }, formattedInputs)
+    }
+
+    while (slotIndex < this.$slots.default.length) {
+      if (this.$slots.default.hasOwnProperty(slotIndex)) {
+        const vNode = this.$slots.default[slotIndex]
+
+        if (vNode.tag === 'label') {
+          if (this.isHorizontal && formattedSlots[fieldLabelCount].length > 1) {
+            fieldLabelCount++
+            formattedSlots.push([])
+          }
+          formattedSlots[fieldLabelCount].push(formatLabels(vNode))
+        } else {
+          if (this.isHorizontal) {
+            formattedSlots[fieldLabelCount].push(concatNextInputs(slotIndex, this.$slots.default))
+          } else {
+            formattedSlots[fieldLabelCount].push(addControl(vNode))
+          }
+        }
+      }
+      slotIndex++
+    }
+
+    if (formattedSlots.length < 2) {
+      return h(
+        'div',
+        {
+          class: [ 'field', rootNodeClass() ]
+        },
+        [
+          formattedSlots[0]
+        ]
+      )
+    } else {
+      return h('div', {}, [
+        formattedSlots.map(groupedSlots => {
+          return h(
+            'div',
+            {
+              class: [ 'field', rootNodeClass() ]
+            },
+            [
+              groupedSlots
+            ]
+          )
+        })
+      ])
+    }
   }
+
 }
 </script>
